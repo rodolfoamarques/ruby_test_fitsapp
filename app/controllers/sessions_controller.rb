f22bfda12b0aaf55f4af15dd5e4ad3873f2d45e7
Rodolfo Almeida Marques <rodolfoalmeidamarques@gmail.com>
2017-01-06 14:20:23 +0000

    Changed landing page after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to chatlist_path
+      redirect_to user_path(user)
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,9 +20,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information" do
     get login_path
     post login_path, params: { session: { email: @user_one.email,  password: 'password' } }
-    assert_redirected_to chatlist_path
+    assert_redirected_to @user_one
     follow_redirect!
-    assert_template 'chats/new'
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", chatlist_path
@@ -32,9 +32,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email: @user_one.email, password: 'password' } }
     assert is_logged_in?
-    assert_redirected_to chatlist_path
+    assert_redirected_to @user_one
     follow_redirect!
-    assert_template 'chats/new'
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", chatlist_path
